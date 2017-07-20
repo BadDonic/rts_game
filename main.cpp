eb94e1c978c56496a15641f959903179488482f3
dzen1k <dzenik1999@gmail.com>
2017-07-20 18:17:15 +0300

    Draw Circle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	RenderWindow window(VideoMode(200, 200), "SMFL works!");
+	CircleShape shape(100.f);
+	shape.setFillColor(Color::Green);
+
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) if (event.type == Event::Closed) window.close();
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
