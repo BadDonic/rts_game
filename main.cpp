42299b9fe6fe3e8065610b451ed0bad085f8053f
dzen1k <dzenik1999@gmail.com>
2017-07-23 19:45:59 +0300

    Basic Work with mouse

--- main.cpp
+++ main.cpp
@@ -12,10 +12,19 @@ int main() {
 	Sprite heroSprite;
 	heroSprite.setTexture(texture);
 	heroSprite.setTextureRect(IntRect(0, 192, 96, 96));
-	heroSprite.setPosition(50, 25);
+	heroSprite.setPosition(250, 250);
+
 	while (window.isOpen()) {
 		Event event;
 		while (window.pollEvent(event)) if (event.type == Event::Closed) window.close();
+
+		if (Keyboard::isKeyPressed(Keyboard::Left)) { heroSprite.move((float)-0.1, 0); heroSprite.setTextureRect(IntRect(0, 96, 96, 96));}
+		if (Keyboard::isKeyPressed(Keyboard::Right)) { heroSprite.move(0.1, 0); heroSprite.setTextureRect(IntRect(0, 192, 96, 96)); }
+		if (Keyboard::isKeyPressed(Keyboard::Up)) { heroSprite.move(0, (float)-0.1); heroSprite.setTextureRect(IntRect(0, 288, 96, 96)); }
+		if (Keyboard::isKeyPressed(Keyboard::Down)) { heroSprite.move(0, 0.1); heroSprite.setTextureRect(IntRect(0, 0, 96, 96)); }
+
+		if (Mouse::isButtonPressed(Mouse::Left)) heroSprite.setColor(Color::Blue);
+		if (Mouse::isButtonPressed(Mouse::Right)) heroSprite.setColor(Color::Red);
 
 		window.clear();
 		window.draw(heroSprite);
