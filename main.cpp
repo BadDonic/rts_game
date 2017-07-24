60aab16892dfff84b88e4c90fa6abcec0131588b
dzen1k <dzenik1999@gmail.com>
2017-07-24 17:38:43 +0300

    Create basic animations

--- main.cpp
+++ main.cpp
@@ -14,17 +14,43 @@ int main() {
 	heroSprite.setTextureRect(IntRect(0, 192, 96, 96));
 	heroSprite.setPosition(250, 250);
 
+	Clock clock;
+	float currentFrame = 0;
+
+
 	while (window.isOpen()) {
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+		time /= 500;
+
+
 		Event event;
 		while (window.pollEvent(event)) if (event.type == Event::Closed) window.close();
 
-		if (Keyboard::isKeyPressed(Keyboard::Left)) { heroSprite.move((float)-0.1, 0); heroSprite.setTextureRect(IntRect(0, 96, 96, 96));}
-		if (Keyboard::isKeyPressed(Keyboard::Right)) { heroSprite.move(0.1, 0); heroSprite.setTextureRect(IntRect(0, 192, 96, 96)); }
-		if (Keyboard::isKeyPressed(Keyboard::Up)) { heroSprite.move(0, (float)-0.1); heroSprite.setTextureRect(IntRect(0, 288, 96, 96)); }
-		if (Keyboard::isKeyPressed(Keyboard::Down)) { heroSprite.move(0, 0.1); heroSprite.setTextureRect(IntRect(0, 0, 96, 96)); }
-
-		if (Mouse::isButtonPressed(Mouse::Left)) heroSprite.setColor(Color::Blue);
-		if (Mouse::isButtonPressed(Mouse::Right)) heroSprite.setColor(Color::Red);
+		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			currentFrame += 0.005 * time;
+			if (currentFrame > 3) currentFrame = 0;
+			heroSprite.setTextureRect(IntRect(96 * (int)currentFrame, 96, 96, 96));
+			heroSprite.move((float)-0.1 * time, 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			currentFrame += 0.005 * time;
+			if (currentFrame > 3) currentFrame = 0;
+			heroSprite.setTextureRect(IntRect(96 * (int)currentFrame, 192, 96, 96));
+			heroSprite.move((float)0.1 * time, 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			currentFrame += 0.005 * time;
+			if (currentFrame > 3) currentFrame = 0;
+			heroSprite.setTextureRect(IntRect(96 * (int)currentFrame, 288, 96, 96));
+			heroSprite.move(0, (float)-0.1 * time);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			currentFrame += 0.005 * time;
+			if (currentFrame > 3) currentFrame = 0;
+			heroSprite.setTextureRect(IntRect(96 * (int)currentFrame, 0, 96, 96));
+			heroSprite.move(0, (float)0.1 * time);
+		}
 
 		window.clear();
 		window.draw(heroSprite);
