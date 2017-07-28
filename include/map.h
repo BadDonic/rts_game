e8e6ea46771cd216818d154802ca2cce029c31ad
dzen1k <dzenik1999@gmail.com>
2017-07-28 13:09:49 +0300

    create class Player

---
+++ include/map.h
@@ -0,0 +1,8 @@
+//
+// Created by daniel on 28.07.17.
+//
+
+#ifndef COURSEWORK_MAP_H
+#define COURSEWORK_MAP_H
+
+#endif //COURSEWORK_MAP_H
--- main.cpp
+++ main.cpp
@@ -3,17 +3,46 @@
 
 using namespace sf;
 
+class Player {
+public:
+	float x, y, width, height, accelX, accelY, speed;
+	int dir;
+	String file;
+	Image image;
+	Texture texture;
+	Sprite sprite;
+	Player(String filePath, float x, float y, float width, float height) {
+		accelX = 0; accelY = 0; speed = 0; dir = 0;
+		file = filePath;
+		this->width = width;
+		this->height = height;
+		this->x = x;
+		this->y = y;
+		image.loadFromFile("../images/" + file);
+		image.createMaskFromColor(Color(41, 33, 59));
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+		sprite.setTextureRect(IntRect(0, 0, this->width, this->height));
+	}
+	void update(float time) {
+		switch (dir) {
+			case 0: accelX = speed; accelY = 0; break;
+			case 1: accelX = -speed; accelY = 0; break;
+			case 2: accelX = 0; accelY = speed; break;
+			case 3: accelX = 0; accelY = -speed; break;
+		}
+		x += accelX * time;
+		y += accelY * time;
+
+		speed = 0;
+		sprite.setPosition(x , y);
+	}
+};
+
 int main() {
 	RenderWindow window(VideoMode(640, 480), "SMFL works!");
 
-	Texture texture;
-	texture.loadFromFile("../images/hero.png");
-
-	Sprite heroSprite;
-	heroSprite.setTexture(texture);
-	heroSprite.setTextureRect(IntRect(0, 192, 96, 96));
-	heroSprite.setPosition(250, 250);
-
+	Player p("hero.png", 250, 250, 96, 96);
 	Clock clock;
 	float currentFrame = 0;
 
@@ -28,32 +57,32 @@ int main() {
 		while (window.pollEvent(event)) if (event.type == Event::Closed) window.close();
 
 		if (Keyboard::isKeyPressed(Keyboard::Left)) {
+			p.dir = 1; p.speed = 0.1;
 			currentFrame += 0.005 * time;
 			if (currentFrame > 3) currentFrame = 0;
-			heroSprite.setTextureRect(IntRect(96 * (int)currentFrame, 96, 96, 96));
-			heroSprite.move((float)-0.1 * time, 0);
+			p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 96, 96, 96));
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Right)) {
+			p.dir = 0; p.speed = 0.1;
 			currentFrame += 0.005 * time;
 			if (currentFrame > 3) currentFrame = 0;
-			heroSprite.setTextureRect(IntRect(96 * (int)currentFrame, 192, 96, 96));
-			heroSprite.move((float)0.1 * time, 0);
+			p.sprite.setTextureRect(IntRect(96 * int(currentFrame),192, 96, 96));
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Up)) {
+			p.dir = 3; p.speed = 0.1;
 			currentFrame += 0.005 * time;
 			if (currentFrame > 3) currentFrame = 0;
-			heroSprite.setTextureRect(IntRect(96 * (int)currentFrame, 288, 96, 96));
-			heroSprite.move(0, (float)-0.1 * time);
+			p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 288, 96, 96));
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Down)) {
+			p.dir = 2; p.speed = 0.1;
 			currentFrame += 0.005 * time;
 			if (currentFrame > 3) currentFrame = 0;
-			heroSprite.setTextureRect(IntRect(96 * (int)currentFrame, 0, 96, 96));
-			heroSprite.move(0, (float)0.1 * time);
+			p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 0, 96, 96));
 		}
-
+		p.update(time);
 		window.clear();
-		window.draw(heroSprite);
+		window.draw(p.sprite);
 		window.display();
 	}
 
