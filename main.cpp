8af4fec607a33837da5dac65c6a967ea1f7ad377
dzen1k <dzenik1999@gmail.com>
2017-08-01 14:14:18 +0300

    delete fuct iteractionWithMap and create CheckCollisionWithMap

--- main.cpp
+++ main.cpp
@@ -8,19 +8,28 @@
 using namespace sf;
 using namespace std;
 
+enum {
+	left,
+	right,
+	up,
+	down,
+	jump,
+	stay
+} state;
+
 class Player {
 private: float x, y;
 public:
 	float width, height, accelX, accelY, speed;
-	int dir, score, health;
-	bool life, isMove, isSelect;
+	int state, score, health;
+	bool life, isMove, isSelect, onGround;
 	String file;
 	Image image;
 	Texture texture;
 	Sprite sprite;
 	Player(String filePath, float x, float y, float width, float height) {
-		accelX = 0; accelY = 0; speed = 0; dir = 0; score = 0; health = 100;
-		life = true; isMove = false; isSelect = false;
+		accelX = 0; accelY = 0; speed = 0; state = 0; score = 0; health = 100;
+		life = true; isMove = false; isSelect = false, onGround = false;
 		file = filePath;
 		this->width = width;
 		this->height = height;
@@ -30,26 +39,61 @@ public:
 		image.createMaskFromColor(Color(0, 0, 255));
 		texture.loadFromImage(image);
 		sprite.setTexture(texture);
-		sprite.setTextureRect(IntRect(0, 0, this->width, this->height));
+		sprite.setTextureRect(IntRect(0, 134, this->width, this->height));
+		sprite.setOrigin(width / 2, height / 2);
+
 	}
 	void update(float time) {
-		switch (dir) {
-			case 0: accelX = speed; accelY = 0; break;
-			case 1: accelX = -speed; accelY = 0; break;
-			case 2: accelX = 0; accelY = speed; break;
-			case 3: accelX = 0; accelY = -speed; break;
+		control();
+		switch (state) {
+			case right: accelX = speed; break;
+			case left: accelX = -speed; break;
+			case up: break;
+			case down: break;
+			case jump: break;
+			case stay: break;
+
 		}
 		x += accelX * time;
+		checkCollisionWithMap(accelX, 0);
 		y += accelY * time;
+		checkCollisionWithMap(0, accelY);
 
-		speed = 0;
-		interactionWithMap();
-		sprite.setPosition(x , y);
-		sprite.setOrigin(width / 2, height / 2);
+		if (!isMove) speed = 0;
+		sprite.setPosition(x + width / 2 , y + height / 2);
 		if (health <= 0) life = false;
+		accelY = accelY + 0.0015 * time;
 	}
 
-	void interactionWithMap() {
+	void control() {
+			if (Keyboard::isKeyPressed(Keyboard::Left)) {
+				state = left;
+//				currentFrame += 0.005 * time;
+//				if (currentFrame > 3) currentFrame = 0;
+//				p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 96, 96, 96));
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Right)) {
+				state = right;
+//				currentFrame += 0.005 * time;
+//				if (currentFrame > 3) currentFrame = 0;
+//				p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 192, 96, 96));
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Up) && onGround) {
+				state = jump; accelY = -0.5; onGround = false;
+//				currentFrame += 0.005 * time;
+//				if (currentFrame > 3) currentFrame = 0;
+//				p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 307, 96, 96));
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Down)) {
+				state = down;
+//				currentFrame += 0.005 * time;
+//				if (currentFrame > 3) currentFrame = 0;
+//				p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 0, 96, 96));
+			}
+		speed = 0.1;
+	}
+
+	void checkCollisionWithMap(float accelX, float accelY) {
 		for (int i = (int)(y / 32); i < (y + height) / 32; ++i) {
 			for (int j = (int)(x / 32); j < (x + width) / 32; ++j) {
 				if (tileMap[i][j] == '0') {
@@ -57,20 +101,7 @@ public:
 					if (accelY < 0) y = i * 32 + 32;
 					if (accelX > 0) x = j * 32 - width;
 					if (accelX < 0) x = j * 32 + 32;
-					return;
-				}
-				if (tileMap[i][j] == 's') {
-					score++;
-					tileMap[i][j] = ' ';
-				}
-				if (tileMap[i][j] == 'h') {
-					health += 20;
-					tileMap[i][j] = ' ';
-				}
-				if (tileMap[i][j] == 'f') {
-					health -= 40;
-					tileMap[i][j] = ' ';
-				}
+				}else onGround = false;
 			}
 		}
 	}
@@ -97,7 +128,7 @@ int main() {
 	Sprite mapSprite;
 	mapSprite.setTexture(mapTexture);
 
-	Player p("heroForRotate.png", 250, 250, 136, 74);
+	Player p("hero.png", 250, 500, 96, 54);
 
 	Font font;
 	font.loadFromFile("../fonts/CyrilicOld.TTF");
@@ -190,48 +221,13 @@ int main() {
 			randomGenerateTimer = 0;
 		}
 
-		if (p.life) {
-			if (Keyboard::isKeyPressed(Keyboard::Left)) {
-				p.dir = 1;
-				p.speed = 0.1;
-				currentFrame += 0.005 * time;
-				if (currentFrame > 3) currentFrame = 0;
-				p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 96, 96, 96));
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Right)) {
-				p.dir = 0;
-				p.speed = 0.1;
-				currentFrame += 0.005 * time;
-				if (currentFrame > 3) currentFrame = 0;
-				p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 192, 96, 96));
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Up)) {
-				p.dir = 3;
-				p.speed = 0.1;
-				currentFrame += 0.005 * time;
-				if (currentFrame > 3) currentFrame = 0;
-				p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 307, 96, 96));
-			}
-			if (Keyboard::isKeyPressed(Keyboard::Down)) {
-				p.dir = 2;
-				p.speed = 0.1;
-				currentFrame += 0.005 * time;
-				if (currentFrame > 3) currentFrame = 0;
-				p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 0, 96, 96));
-			}
-			//setPlayerCoordinatesForView(p.getPlayerCoordinateX(), p.getPlayerCoordinateY());
-		}else {
-			view.move(0.1, 0);
-		}
-
-
-
 		Vector2i localPosition = Mouse::getPosition(window);
 		if (localPosition.x < 3) view.move(-0.2 * time, 0);
 		if (localPosition.x > window.getSize().x - 73) view.move(0.2 * time, 0);
 		if (localPosition.y > window.getSize().y - 58) view.move(0, 0.2 * time);
 		if (localPosition.y < 3) view.move(0, -0.2 * time);
 
+		if (p.life) setPlayerCoordinatesForView(p.getPlayerCoordinateX(), p.getPlayerCoordinateY());
 
 		p.update(time);
 
